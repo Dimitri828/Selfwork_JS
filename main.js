6544fd046db5f4b9d8cba58916ae49f2124ba0d5
Dimitri <dimitrifortini91@gmail.com>
2026-04-21 23:17:21 +0200

    eseguito selfJS 2

---
+++ main.js
@@ -0,0 +1,38 @@
+//  let num1 = 5;
+//     let num2 = 10;
+
+//     console.log(num1);
+//     console.log(num2);
+
+//     let stringa = "Buonasera";
+//     console.log(stringa);
+//     stringa = 88;
+//     console.log(stringa);
+
+//     const pigreco = 3.14159265359;
+
+//     console.log(pigreco);
+
+//     pigreco = 38;
+    
+
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+
+
+
+let somma = a  + b + c + d + e
+console.log(`La somma tra i numeri equivale a ${somma}`);
+
+let media = (a  + b + c + d + e)/5
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
+
+
+
