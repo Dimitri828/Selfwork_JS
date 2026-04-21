f5f83b73b0dd4588b031bf66f856c8a8d71f2034
Dimitri <dimitrifortini91@gmail.com>
2026-04-21 23:29:21 +0200

    eseguito selfJS 3

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+// SELFWORK 1
+
 //  let num1 = 5;
 //     let num2 = 10;
 
@@ -15,24 +18,40 @@
 
 //     pigreco = 38;
     
+// SELFWORK 2
+
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
 
 
 
-let a = 1;
-let b = 2;
-let c = 3;
-let d = 4;
-let e = 5;
+
+// let somma = a  + b + c + d + e;
+// console.log(`La somma tra i numeri equivale a ${somma}`);
+
+// let media = (a  + b + c + d + e)/5;
+
+// console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
 
 
+// SELFWORK 3
+
+let currentAge = 2026;
+
+let birthdayAge = 1991;
+
+let age = currentAge - birthdayAge;
+
+console.log(age);
+
+let hundred = 100 - age
+
+console.log(hundred);
 
 
-let somma = a  + b + c + d + e
-console.log(`La somma tra i numeri equivale a ${somma}`);
-
-let media = (a  + b + c + d + e)/5
-
-console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
-
-
+console.log(`Hai ${age} anni e ti mancano ${hundred} anni per compierne 100`);
 
